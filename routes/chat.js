b20d79a514b57d481871c1fc94e17d97d3c0479e
Lusketa000 <lucascamargo2004@gmail.com>
2025-04-22 14:48:48 -0300

    Chat Bonito✨

--- routes/chat.js
+++ routes/chat.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.render("chat/menu", {layout:"layouts/chats"});
+});
+
 // Página do chat (simulação de conversa entre 2 usuários)
 router.get("/:roomId", (req, res) => {
     const roomId = req.params.roomId;
     const user = { id: req.session.userId || 1 }; // Simula ID do usuário logado
-    res.render("chat/chat", { roomId, user , layout:"layouts/basic"});
+    res.render("chat/chat", { roomId, user , layout:"layouts/chats"});
 });
 
 module.exports = router;
