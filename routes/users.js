9835163c611d05cc33cd94668fdf82597e667bf7
Asdfghc <lucashmeng@gmail.com>
2025-04-23 15:57:08 -0300

    Revert "Merge branch 'chat' of https://github.com/Asdfghc/Aplicativo into chat"
    This reverts commit 59745261ccd7bea9acbe1e26dee062b006bf5cf5, reversing
    changes made to 2e463d070c1e6b9a27ce13e816e083454bd5abf3.

--- routes/users.js
+++ routes/users.js
@@ -23,10 +23,6 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/self", checkAuthenticated, async (req, res) => {
-        res.redirect("/users/"+ req.user.id);
-});
-
 router.post("/new", async (req, res) => {
     let { name, email, CPF, password } = req.body;
 
