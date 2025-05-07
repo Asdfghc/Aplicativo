c09a2654ec0c3b2a69d73e8af76d76d7103d2c2f
RAFAEL SISCARO DE CARVALHO <rafael.sc3@puccampinas.edu.br>
2025-05-07 10:57:30 -0300

    v0.2.3 Formulário de criação do post

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { DateTime } = require('luxon');
 
 // GET
 router.get("/create", checkAuthenticated, async (req, res) => {
@@ -10,22 +11,35 @@ router.get("/create", checkAuthenticated, async (req, res) => {
 router.post("/create", checkAuthenticated, async (req, res) => {
     let { description, lostOrFound, lastSeen, dateLost } = req.body;
 
-    if (!description || !lostOrFound || !lastSeen || !dateLost) {
+    if (!description || !lastSeen || !dateLost) {
         req.flash("error", "Preencha todos os campos.");
         return res.redirect("/posts/create");
     }
 
+    if (!lostOrFound) {
+        lostOrFound = 0;
+    }
+
+    // Converter a data para o formato correto
+    const formattedDateLost = DateTime.fromISO(dateLost).toFormat('yyyy-MM-dd HH:mm');
+
     try {
         await req.db.execute(
-            "INSERT INTO Postagem (Descricao, Achado_ou_perdido, Ultimo_local_visto, Data_quando_perdeu) VALUES (:description, :lostOrFound, :lastSeen, :dateLost)",
-            [description, lostOrFound, lastSeen, dateLost]
+            "INSERT INTO Postagem (Descricao, Achado_ou_perdido, Ultimo_local_visto, Data_quando_perdeu, ID_Usuario) VALUES (:description, :lostOrFound, :lastSeen, TO_DATE(:dateLost, 'YYYY-MM-DD HH24:MI'), :userId)",
+            {
+                description: description,
+                lostOrFound: lostOrFound,
+                lastSeen: lastSeen,
+                dateLost: formattedDateLost,
+                userId: req.user.id
+            }
         );
         await req.db.commit();
         req.flash("success", "Postagem criada com sucesso.");
         res.redirect("/posts/create");
     } catch (err) {
         console.error("Erro ao criar postagem:", err);
-        req.flash("error", "Erro ao criar postagem.");
+        req.flash("error", "Erro ao criar postagem: " + err.message);
         res.redirect("/posts/create");
     }
 });
